eeb8975093cbc235e76ae16ad9f8e83813f5d812
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2022-05-25 16:39:03 -0300

    Done the first Lab about Collections

---
+++ src/com/lab/view/Cap15_Lab01.java
@@ -0,0 +1,18 @@
+package com.lab.view;
+
+import java.util.HashMap;
+
+public class Cap15_Lab01 {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> pessoaMap = new HashMap<>();
+		pessoaMap.put("Roberto", 35);
+		pessoaMap.put("Carla", 26);
+		pessoaMap.put("Gabriel", 18);
+		pessoaMap.put("Bruna", 17);
+
+		System.out.println("-- Pessoas --");
+		pessoaMap.forEach((k,v) -> System.out.println(k +": "+ v));
+	}
+
+}
